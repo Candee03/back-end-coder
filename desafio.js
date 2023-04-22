e421e7b0bca67dec148b06a62a31ae7f65d9d7d6
Candela Alfano <candela.alfano1503@gmail.com>
2023-04-22 14:52:56 -0300

    correcion function update

--- desafio.js
+++ desafio.js
@@ -1,8 +1,12 @@
-class ProductManager {
+const fs = require('fs')
+
+class ProductManager{
+    constructor(path) {
+        this.path = path
+        this.products = fs.promises.writeFile (this.path, JSON.stringify([]))
+    }
+
     #id = 0
-
-    products = []
-
     #getId () {
         this.#id++
         return this.#id
@@ -16,83 +20,127 @@ class ProductManager {
 	 * @param {string} code Código identificador
 	 * @param {number} stock Número de piezas disponibles
      */
+    async addProduct (title, description, price, thumbnail, code, stock) {
+        try {
+            const product = {
+                title,
+                description,
+                price,
+                thumbnail,
+                code,
+                stock
+            }
+            if (product.title === undefined || product.code === undefined || product.stock === undefined) return err
+            
+            const actualListProducts = await this.getProducts()
+            let codigoRepetido = actualListProducts.find(p => code === p.code)
 
-    addProduct (title, description, price, thumbnail, code, stock) {
-        const product = {
-            title,
-            description,
-            price,
-            thumbnail,
-            code,
-            stock
+            if (codigoRepetido) {
+                console.log('ERROR: Código repetido');
+                return err
+            } else {
+                product.id = this.#getId()
+                actualListProducts.push(product)
+                await fs.promises.writeFile (this.path, JSON.stringify(actualListProducts))
+            }
         }
-        let codigoRepetido = this.products.find(p => code === p.code)
-        if (codigoRepetido) {
-            console.log('ERROR: Código repetido');
-        } else {
-            this.products.push(product)
-            product.id = this.#getId()
+        catch (err) {
+            console.log(`No se pudo agregar`);
         }
     }
 
-    getProductById (id) {
-        let productoRepetido = this.products.find(p => id === p.id)
-        if (productoRepetido){
-            console.log(productoRepetido);
-        } else {
+    /**
+    * @param {number} id Id del producto a eliminar
+    */
+    async deleteProduct (id) {
+        try{
+            const actualListProducts = JSON.parse(await fs.promises.readFile(this.path, 'utf-8'))
+            let newList = actualListProducts.filter(p => p.id !== id)
+            if (newList.length === actualListProducts.length) {
+                return err
+            } else {
+                await fs.promises.writeFile (this.path, JSON.stringify(newList))
+            }
+        } catch (err) {
+            console.log('No se pudo borrar');
+        }
+    }
+
+    /**
+     * @param {number} id Id del producto a eliminar
+     * @param {string} title Nombre del producto
+	 * @param {string} description Descripción del producto
+     * @param {number} price Precio
+	 * @param {string} thumbnail Ruta de imagen
+	 * @param {number} stock Número de piezas disponibles
+    */
+    async updateProduct (id, updatedProduct) {
+        try {
+            const actualListProducts = await this.getProducts()
+            const product = await actualListProducts.find(p => id === p.id)
+            let index = await actualListProducts.findIndex((e) => e.id === product.id)
+
+            const newProduct = {
+                ...product,
+                ...updatedProduct,
+            }
+
+            actualListProducts.splice(index, 1, newProduct)
+            await fs.promises.writeFile (this.path, JSON.stringify(actualListProducts))
+        }
+        catch (err) {
+            console.log(`No se puede modificar el producto con id ${id} porque no existe`);
+        }
+    }
+
+    /**
+     * @param {number} id 
+     * @returns productoEncontrado
+     */
+    async getProductById (id) {
+        try{
+            const products = JSON.parse(await fs.promises.readFile(this.path, 'utf-8'))
+            let productoEncontrado = products.find(p => id === p.id)
+            if (productoEncontrado === undefined) {
+                return err
+            } else {
+                return productoEncontrado
+            }
+        } catch (err) {
             console.log('Not Found');
         }
     }
 
-    getProducts () {
-        console.log(this.products);
+    async getProducts () {
+        try{
+            const products = JSON.parse(await fs.promises.readFile(this.path, 'utf-8'))
+            return products
+        } catch (err) {
+            console.log(err);
+        }
     }
 }
 
 
 //!-----------Pruebas------------
 
-// const products = new ProductManager()
+// const products = new ProductManager('./productos.json')
 
-const listProducts = [
-    {
-        nombre: 'fideos',
-        description: 'paquete',
-        precio: 200,
-        img: 'imagen',
-        codigo: 'A54FT',
-        stock: 3
-    },
-    {
-        nombre: 'arroz',
-        description: 'paquete',
-        precio: 400,
-        img: 'imagen',
-        codigo: 'BFF456',
-        stock: 4
-    },
-    {
-        nombre: 'polenta',
-        description: 'paquete',
-        precio: 300,
-        img: 'imagen',
-        codigo: 'PRS56T',
-        stock: 5
-    }
-]
+// setTimeout(()=>{test()},3000)
 
-const agregar = () => {
-    listProducts.map((p) => {
-        products.addProduct(p.nombre, p.description, p.precio, p.img, p.codigo, p.stock)
-    })
-} 
-agregar()
+// const test = async () => {
+//     // console.log(await products.getProducts());
 
-
-
-// products.getProducts();
-
-// products.addProduct('fideos', 'paquete', 200, 'imagen', 'A54FT', 3)
-
-// products.getProductById(1);
-// products.getProductById(6);
+//     await products.addProduct('fideos', 'paquete', 200, 'imagen', 'A54FT', 3)
+//     await products.addProduct('arroz', 'paquete', 600, 'imagen', 'PFFG45', 5)
+//     await products.addProduct('polenta', 'paquete', 300, 'imagen', 'DFR56', 5)
+    
+//     // console.log(await products.getProducts());
+    
+//     // console.log(await products.getProductById(1))
+//     // console.log(await products.getProductById(6))
+//     await products.updateProduct(1,{title:'fideitos', price:500, stock:6})
+    
+//     // await products.deleteProduct(1)
+//     console.log(await products.getProducts());
+// }
