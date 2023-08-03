5b960c99f3fc99466b71f9cbc43be9f5528b925d
Candela Alfano <candela.alfano1503@gmail.com>
2023-08-03 18:52:59 -0300

    delete midllewares

--- src/middleware/auth.middleware.js
+++ src/middleware/auth.middleware.js
@@ -1,17 +1,3 @@
-// export function allowedModifyCart (req, res, next) {
-// 	const user = req.session.user
-// 	if (!user) res.status(401).send({error:'primero tienes que iniciar sesion'})
-// 	if (user.cartId._id !== req.params.cid) res.status(401).send({error:'no tienes permiso agregar prductos a este carrito'})
-// 	else next()
-// }
-
-// export function allowedModifyProducts (req, res, next) {
-// 	const user = req.session.user
-// 	if (!user) res.status(401).send({error:'primero tienes que iniciar sesion'})
-// 	if (user.role !== 'admin') res.status(401).send({error:'no tienes permiso para modificar la lista de productos'})
-// 	else next()
-// }
-
 export function isGuest (req, res, next) {
 	if (!req.cookies.token) {
 		next();
