85c578d53eb677dd824feb995741deb4c2c9dbdf
Candela Alfano <candela.alfano1503@gmail.com>
2023-10-06 15:01:13 -0300

    fix

--- public/js/Purchase.js
+++ public/js/Purchase.js
@@ -1,5 +1,5 @@
 async function purchase (cid) {
-    await fetch (`http://localhost:8080/api/carts/${cid}/purchase`, {method: 'POST'})
+    await fetch (`/api/carts/${cid}/purchase`, {method: 'POST'})
     .then((res) => {
         if (res.ok) location.replace('/products')
     })
